6b7f8497fdf4986439206b7fb5a2d846990bcd00
Mohammed Majid <majidmohammed044@gmail.com>
2025-02-22 15:51:00 +0000

    rendered new styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './styles.css';
+
+document.getElementById('menu-btn').addEventListener('click', function() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.style.transform = sidebar.style.transform === 'translateX(0px)' ? 'translateX(-100%)' : 'translateX(0px)';
+});
